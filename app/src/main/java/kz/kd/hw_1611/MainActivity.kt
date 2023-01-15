083377ae0a5ab0b402d36cb0c37c6cff6420eaf5
krymov.dulat <krymov.dulat@gmail.com>
2023-01-15 14:53:44 +0600

    Added OnSave and OnRestore, position is saved after orientation change, more items for testing

--- app/src/main/java/kz/kd/hw_1611/MainActivity.kt
+++ app/src/main/java/kz/kd/hw_1611/MainActivity.kt
@@ -1,9 +1,13 @@
 package kz.kd.hw_1611
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+
+private const val KEY_VISIBLE_ITEM = "Visible Item"
+private const val KEY_COMPLETELY_VISIBLE_ITEM = "Completely Visible Item"
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +20,34 @@ class MainActivity : AppCompatActivity() {
 
         initCurrencyRecycler()
         fillCurrency()
+
+        if (savedInstanceState != null) {
+            val firstVisibleItem = savedInstanceState.getInt(KEY_VISIBLE_ITEM)
+            val firstCompletelyVisibleItem = savedInstanceState.getInt(KEY_COMPLETELY_VISIBLE_ITEM)
+            currencyLayoutManager.scrollToPositionWithOffset(
+                firstVisibleItem,
+                firstCompletelyVisibleItem
+            )
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        val firstVisibleItem = currencyLayoutManager.findFirstVisibleItemPosition()
+        val firstCompletelyVisibleItem =
+            currencyLayoutManager.findFirstCompletelyVisibleItemPosition()
+        outState.putInt(KEY_VISIBLE_ITEM, firstVisibleItem)
+        outState.putInt(KEY_COMPLETELY_VISIBLE_ITEM, firstCompletelyVisibleItem)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        val firstVisibleItem = savedInstanceState.getInt(KEY_VISIBLE_ITEM)
+        val firstCompletelyVisibleItem = savedInstanceState.getInt(KEY_COMPLETELY_VISIBLE_ITEM)
+        currencyLayoutManager.scrollToPositionWithOffset(
+            firstVisibleItem,
+            firstCompletelyVisibleItem
+        )
+        super.onRestoreInstanceState(savedInstanceState)
     }
 
     private fun initCurrencyRecycler() {
@@ -62,6 +94,54 @@ class MainActivity : AppCompatActivity() {
                 flag = R.drawable.ic_eu,
                 country = getString(R.string.eu),
                 currencyName = getString(R.string.eu_currency)
+            )
+        )
+        currencyList.add(
+            Currency(
+                amount = "5",
+                flag = R.drawable.ic_usa,
+                country = getString(R.string.usa),
+                currencyName = getString(R.string.usa_currency)
+            )
+        )
+        currencyList.add(
+            Currency(
+                amount = "6",
+                flag = R.drawable.ic_kz,
+                country = getString(R.string.kz),
+                currencyName = getString(R.string.kz_currency)
+            )
+        )
+        currencyList.add(
+            Currency(
+                amount = "7",
+                flag = R.drawable.ic_usa,
+                country = getString(R.string.usa),
+                currencyName = getString(R.string.usa_currency)
+            )
+        )
+        currencyList.add(
+            Currency(
+                amount = "8",
+                flag = R.drawable.ic_tr,
+                country = getString(R.string.tr),
+                currencyName = getString(R.string.tr_currency)
+            )
+        )
+        currencyList.add(
+            Currency(
+                amount = "9",
+                flag = R.drawable.ic_eu,
+                country = getString(R.string.eu),
+                currencyName = getString(R.string.eu_currency)
+            )
+        )
+        currencyList.add(
+            Currency(
+                amount = "10",
+                flag = R.drawable.ic_usa,
+                country = getString(R.string.usa),
+                currencyName = getString(R.string.usa_currency)
             )
         )
         currencyAdapter.updateDataSet(currencyList)
